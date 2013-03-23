3f603df1ae86ea501c7e9d2740e9814500edc89a
Gleicon Moraes <gleicon@gmail.com>
2013-03-23 09:13:44 -0300

    took out omnibus

--- cookbooks/docker/recipes/default.rb
+++ cookbooks/docker/recipes/default.rb
@@ -1,7 +1,11 @@
-package 'bsdtar' do
+package "bsdtar" do
     action :install
 end
 
+package "linux-image-extra-virtual" do
+    action :install
+end
+ 
 remote_file "/tmp/docker-master.tgz" do
     os = `uname -s`.strip
     arch = `uname -m`.strip
